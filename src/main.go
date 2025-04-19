ff334bce43e325be254c3362a8f61c4505772117
ReesvGupta <guptareesav16@gmail.com>
2025-04-19 21:46:57 +0530

    init commit

---
+++ src/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// Usage: your_program.sh <command> <arg1> <arg2> ...
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: mygit <command> [<args>...]\n")
+		os.Exit(1)
+	}
+
+	switch command := os.Args[1]; command {
+	case "init":
+		InitRepository()
+	case "cat-file":
+		CatFile()
+	default:
+		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
+		os.Exit(1)
+	}
+}
